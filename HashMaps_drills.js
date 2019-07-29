7bf04442c06eb9c8f5e8a9746a1057f39008247d
Lucas Vocos <lucas.vocos@me.com>
2019-07-29 12:47:03 -0400

    exercises 1-3

---
+++ HashMaps_drills.js
@@ -0,0 +1,63 @@
+const HashMap = require('./hashmap.js')
+function main() {
+  const newHashMap = new HashMap()
+
+  newHashMap.MAX_LOAD_RATIO = 0.5
+  newHashMap.SIZE_RATIO = 3
+
+  newHashMap.set('Hobbit', 'Bilbo')
+  newHashMap.set('Hobbit', 'Frodo')
+  newHashMap.set('Wizard', 'Gandalf')
+  newHashMap.set('Human', 'Aragon')
+  newHashMap.set('Elf', 'Legolas')
+  newHashMap.set('Maiar', 'The Necromancer')
+  newHashMap.set('Maiar', 'Sauron')
+  newHashMap.set('RingBearer', 'Gollum')
+  newHashMap.set('LadyOfLight', 'Galadriel')
+  newHashMap.set('HalfElven', 'Arwen')
+  newHashMap.set('Ent', 'Treebeard')
+
+  console.log(newHashMap._hashTable);
+
+  // Exercise 1
+
+  // The set() function is looking for the key.
+  // Because both Bilbo and Frodo have the same key,
+  // as well as Necromancer and Sauron,
+  // the values are being overwritten
+
+  // It's checking if the key is in the box, not the value
+
+  // Exercise 2
+    const WhatDoesThisDo = function(){
+      let str1 = 'Hello World.';
+      let str2 = 'Hello World.';
+      let map1 = new HashMap();
+      map1.set(str1,10);
+      map1.set(str2,20);
+      // map1 might just have one key but with the value of 20
+      // since it was overwritten
+      let map2 = new HashMap();
+      let str3 = str1;
+      let str4 = str2;
+      map2.set(str3,20);
+      map2.set(str4,10);
+      // This will also just have one key, and the value would be 10
+      // since it was also overwritten
+
+      console.log(map1.get(str1));
+      // This returns "20"
+      console.log(map2.get(str3));
+      // This returns "10"
+
+
+
+    // Exercise 3
+    // 1: [22, 88, undefined, undefined, 4, 15, 28, 17, 59, 31, 10]
+    // 2: [undefined, 28 -> 19 -> 10, 20, 12, undefined, 5, 15 -> 33, undefined, 17]
+  }
+
+
+}
+
+main()
